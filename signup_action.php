c055b441f72cd58186fdf38ad4e6193f5f747064
msadhimb <adhimbaqy72@gmail.com>
2022-06-12 15:35:02 +0700

    Revisi Admin

--- signup_action.php
+++ signup_action.php
@@ -11,14 +11,7 @@ $cpasswd = $_POST['cPassword'];
 if ($passwd == $cpasswd) {
     $psw = password_hash($passwd, PASSWORD_DEFAULT);
     $data = [$uname, $email, $psw];
-
-    $namaPisah = explode("@", $email);
-    $namaPisah2 = explode(".", $namaPisah[1]);
-    if ($namaPisah2[0] === "admin") {
-        $d->insertAdmin($data);
-    } else if ($namaPisah2[0] === "gmail") {
-        $d->insertData($data);
-    }
+    $d->insertData($data);
 
     header("Location: index.php?msg=success");
 } else {
